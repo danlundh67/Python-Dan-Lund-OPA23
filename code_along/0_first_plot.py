97768d0ec9eb9fb1fae038c07e444f5271649438
Dan Lund <dan.lundh@outlook.com>
2024-02-05 14:19:44 +0100

    matplotlib test

---
+++ code_along/0_first_plot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt 
+
+numbers=list(range(10))
+squares=[number**2 for number in numbers]
+
+print(numbers)
+print(squares)
+
+plt.plot(numbers, squares)
+plt.title("x^2 for positive integers 0-9")
+plt.xlabel("x")
+plt.ylabel("x^2")
+plt.show()
